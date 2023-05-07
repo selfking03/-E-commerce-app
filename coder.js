3885029079364c40095dc71062530e73fa91a045
selfking <kazeemolayiwola1998@gmail.com>
2023-05-07 00:54:59 -0700

    the comment of what you did

--- coder.js
+++ coder.js
@@ -2,7 +2,7 @@ var MenuItems = document.getElementById(MenuItems);
 MenuItems.style.maxHeight = "0px";
 function menutoggle() {
     if(MenuItems.style.maxHeight == "0px") {
-        MenuItems.style,maxHeight = "200px";
+        MenuItems.style.maxHeight = "200px";
     }
     else
     {
